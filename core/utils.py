16a2a4a48c1cea61d8e8143fadbced168fc92df7
mikeleg <g.mikele@gmail.com>
2020-09-30 12:47:04 +0200

    Some agustment

--- core/utils.py
+++ core/utils.py
@@ -1,9 +1,10 @@
 import os
 
-
 def get_env_key(key_name: str) -> str:
-
-    if os.environ[key_name] is None:
+    try:
+        return os.environ.get(key_name)
+    except KeyError:
         raise EnvironmentError(f"{key_name} is not in enviroment")
 
-    return os.environ[key_name]
+
+    
